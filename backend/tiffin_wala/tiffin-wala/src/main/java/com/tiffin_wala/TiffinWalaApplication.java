6300c8de51dfd69d9d459400b040b83dbdd9b4b3
Tikling-Mind <rohandehankar10@gmail.com>
2023-08-15 15:47:46 +0530

    Created spring boot app for the backend

---
+++ backend/tiffin_wala/tiffin-wala/src/main/java/com/tiffin_wala/TiffinWalaApplication.java
@@ -0,0 +1,13 @@
+package com.tiffin_wala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffinWalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiffinWalaApplication.class, args);
+	}
+
+}
