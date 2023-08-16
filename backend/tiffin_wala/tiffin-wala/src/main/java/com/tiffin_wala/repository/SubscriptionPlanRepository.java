5e70b16fbc8a874512e63bc6e73a9c57ea09a00f
Pradip Borade <pradipborade911@gmail.com>
2023-08-16 20:53:43 +0000

    Added service implementation

---
+++ backend/tiffin_wala/tiffin-wala/src/main/java/com/tiffin_wala/repository/SubscriptionPlanRepository.java
@@ -0,0 +1,9 @@
+package com.tiffin_wala.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tiffin_wala.entities.SubscriptionPlan;
+
+public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Long> {
+
+}
