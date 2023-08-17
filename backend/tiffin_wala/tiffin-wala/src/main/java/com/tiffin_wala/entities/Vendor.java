a5229e6463d606827566edaf51449b822d191b59
suraj77jr <suraj77jr@gmail.com>
2023-08-17 20:13:19 +0530

    Added Entities and Enums

--- backend/tiffin_wala/tiffin-wala/src/main/java/com/tiffin_wala/entities/Vendor.java
+++ backend/tiffin_wala/tiffin-wala/src/main/java/com/tiffin_wala/entities/Vendor.java
@@ -1,5 +1,48 @@
 package com.tiffin_wala.entities;
 
-public class Vendor {
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "vendors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vendor extends BaseEntity {
+
+	private boolean isVerified;
+	private boolean isAvailable;
+	private boolean isBlocked;
+	
+	String firstName;
+	String lastName;
+	String email;
+	String mobile;
+	LocalDate registerDate;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="address_id")
+	private Address address;
+	
+//	@JsonManagedReference
+//	@OneToMany(mappedBy = "vendor", cascade = CascadeType.ALL, orphanRemoval = true)
+//	private Set<SubscriptionPlan> plans = new HashSet<SubscriptionPlan>();
+
 
 }
