3c5cff5419fff2f6735880817e8a4afc60d3d534
Tikling-Mind <rohandehankar10@gmail.com>
2023-08-22 20:21:27 +0530

    Modified Entities

--- backend/tiffin_wala/tiffin-wala/src/main/java/com/tiffin_wala/repository/CustomerSubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiffin_wala.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tiffin_wala.entities.CustomerSubscription;
-
-public interface CustomerSubscriptionRepository extends JpaRepository<CustomerSubscription, Long> {
-
-}
