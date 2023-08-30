a9db36fd9b7e09a9cdd5d3318d3ed0979bb4529d
Pradip Borade <pradipborade911@gmail.com>
2023-08-30 21:42:10 +0530

    updated VendorTiffinList, VendorUpdate

--- frontend/tiffin_wala/src/components/pages/vendor/VendorTiffinList.jsx
+++ frontend/tiffin_wala/src/components/pages/vendor/VendorTiffinList.jsx
@@ -1,17 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import VendorList from "./VendorList"
+import TiffinService from '../../../service/TiffinService';
+import { useNavigate } from 'react-router-dom';
+import swal from "sweetalert";
 
 
 
 const VendorTiffinList = (props) => {
-    return (
-        <div>
-        <div class="d-flex flex-row-reverse">
-            <div className='flex-fill mx-2'><VendorList /></div>
-        </div>
+    const [tiffinList, setTiffinList] = useState([]);
+
+    const [vendor, setVendor] = useState({
+        email: "",
+        id: "",
+        jwt: ""
+    });
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        let vend = JSON.parse(sessionStorage.getItem("vendor"));
+        if (vend == null) {
+            //swal("Not Authorized", "", "error");
+        }
+        else {
+            setLoggedIn(true);
+            setVendor({
+                id: vend.id,
+                email: vend.email,
+                jwt: vend.jwt
+            })
+        }
         
+
+        TiffinService.getTiffinsByVendorId(vendor.id)
+            .then(res => {
+                console.log(res.data);
+                setTiffinList(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+                swal(err);
+            })
+    }, []);
+
+    const updateTiffin = (tiffin) => {
+        let cust = JSON.parse(sessionStorage.getItem("vendor"))
+        if (cust == null)
+            swal("Please login to update tiffin", "", "error")
+        else {
+            navigate("/placeOrder");
+        }
+
+    }
+
+    return (loggedIn ? (<>
+        <div className="container my-4">
+            <div>
+                <h3>All available tiffins</h3>
+
+                <table className="table table-btiffined" style={{ textAlign: "center" }}>
+                    <thead className="bg-dark text-light">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Food Type</th>
+                            <th>Breakfast</th>
+                            <th>Lunch</th>
+                            <th>Dinner  </th>
+                            <th>Description</th>
+                            <th>Available from</th>
+                            <th>Available Till</th>
+                            <th>Vendor</th>
+                            <th>Price</th>
+                            <th>Update Tiffin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tiffinList.map((tiffin, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{tiffin.id}</td>
+                                    <td>{tiffin.name}</td>
+                                    <td>{tiffin.foodType}</td>
+                                    <td>{Math.floor(tiffin.breakLunchDinner / 100) % 10 ? "Yes" : "No"}</td>
+                                    <td>{Math.floor(tiffin.breakLunchDinner / 10) % 10 ? "Yes" : "No"}</td>
+                                    <td>{tiffin.breakLunchDinner % 10 ? "Yes" : "No"}</td>
+                                    <td>{tiffin.description}</td>
+                                    <td>{tiffin.availableFrom}</td>
+                                    <td>{tiffin.availableTo}</td>
+                                    <td>{tiffin.vendor.firstName}</td>
+                                    <td>{tiffin.price}</td>
+                                    <td>
+                                        <button className="btn btn-primary" onClick={() => updateTiffin(tiffin)}> Update </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+    </>) : <div style={{ textAlign: "center" }}><h1>Please Log in to Access this page</h1></div>);
 
 }
 
